a391a1680e17fe6de65ec0057fb7b0a4a2ea7ac1
Brandon Chanderban <bchanderban@gmail.com>
2024-01-16 16:50:27 -0400

    initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,12 @@
+/// Import Map Configuration (config/importmap.rb)
+import "@hotwired/turbo-rails"
+import "controllers"
+
+// Additional Dependencies
+import "jquery" // If your project uses jQuery
+import "bootstrap" // If your project uses Bootstrap JavaScript components
+
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
+
